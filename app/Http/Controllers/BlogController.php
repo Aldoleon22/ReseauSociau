e8ac3fa3279f2965bf9332c600776ba10b8ff0eb
Aldo Leon <leonaldo2608@gmail.com>
2024-04-25 15:28:26 +0300

    maj

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,32 @@
+namespace App\Http\Controllers;
+
+
+use App\Models\Post;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Http\RedirectResponse;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+
+{
+
+    public function index (): Paginator {
+        $posts = \App\Hode1s\Post :: paginate(25);
+
+return view('blog.index");|
+
+public function show(string $slug, string $id): RedirectResponse | Post
+{
+$post = \App\Models\Post :: findOrFail ($id);
+if ($post->slug !== $slug) {
+return to_route('blog.show', ['slug' = $post—slug, ‘id' = $post—id]);
+}
+
+return $post;
+}
+
+
+}
+
+}
---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,5 @@
+@extends('base')
+@section('content')
+
+<h1>Mon reseau sociau</h1>
+@endsection
